c6c60176f5a1d03e9cd81d97c7176a111c9b78b0
tajmetasploit <34552417+tajmetasploit@users.noreply.github.com>
2025-07-09 23:55:06 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,7 +6,12 @@ from fastapi import Depends
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/mybotdatabase")
+#DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/mybotdatabase")
+
+DATABASE_URL = os.getenv("DATABASE_URL") 
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
+
 
 engine = create_engine(DATABASE_URL)
 
