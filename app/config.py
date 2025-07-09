33f1a0d48742232c6198d245ad023148f7bfba32
tajmetasploit <34552417+tajmetasploit@users.noreply.github.com>
2025-07-10 02:20:45 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,2 +1,10 @@
+#import os
+#TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "8058688084:AAG0LreV_E0vaQPqEW9QC9-TYRCDgp4lyp4")
+
 import os
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "8058688084:AAG0LreV_E0vaQPqEW9QC9-TYRCDgp4lyp4")
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
