6ba274bce92eaea3afaa532328d20c34233ceba5
taj <tajhackerone@gmail.com>
2025-07-11 15:31:40 +0300

    Updated bot

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ from sqlalchemy import Column, Integer, String
 from app.database import Base
 
 class Message(Base):
-    __tablename__ = "messages"
+    __tablename__ = "message"
 
     id = Column(Integer, primary_key=True, index=True)
     text = Column(String, index=True)
