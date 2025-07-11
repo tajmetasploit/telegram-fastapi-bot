766172aadb8084cadf0609bd0631cc7daee7c2cf
taj <tajhackerone@gmail.com>
2025-07-11 12:22:30 +0300

    Prepare for Railway deploy

--- app/database.py
+++ app/database.py
@@ -104,7 +104,7 @@ def get_db():
     finally:
         db.close()"""
 
-import os
+"""import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -133,9 +133,9 @@ def get_db():
         yield db
     finally:
         db.close()
+"""
 
-
-"""from sqlalchemy import create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
@@ -161,4 +161,3 @@ def get_db():
         yield db
     finally:
         db.close()
-"""
